b0e4b67594ad5b4a62082c8d72b9fa3e62935746
serge_kamya <sergekamya@gmail.com>
2021-08-15 20:03:01 +0200

    added code to app.js that connects to local storage host

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -17,7 +17,7 @@ describe('user can create a contact', () => {
     });
 
     it('displays the phone number of the new contact', () => {
-        cy.get("contact-list").should("contain", "0700 101010")
+        cy.get("#contact-list").should("contain", "0700 101010")
               
     });
     
