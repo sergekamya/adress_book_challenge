8b961abe2213d4b2224e81f9c49d976e76e60067
serge_kamya <sergekamya@gmail.com>
2021-08-15 18:21:50 +0200

    added method to display name and phone number in userCanCreate file

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -8,6 +8,17 @@ describe('user can create a contact', () => {
         cy.get("#company").type("Craft Academy")
         cy.get("#notes").type("Awesome coder")
         cy.get("#twitter").type("@thomasochman")
+        cy.get("#submit").click()
+    });
+
+    it('displays a name of the new contact', () => {
+        cy.get("#contact-list").should("contain", "Thomas")
+
+    });
+
+    it('displays the phone number of the new contact', () => {
+        cy.get("contact-list").should("contain", "0700 101010")
+              
     });
     
 });
